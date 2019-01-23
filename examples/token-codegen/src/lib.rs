e8dc837a892d7754ff71e9518d807041738aae35
lucas7788 <sishsh@163.com>
2019-01-23 19:05:28 +0800

    add approve allowance transfer_from and test

--- examples/token-codegen/src/lib.rs
+++ examples/token-codegen/src/lib.rs
@@ -10,6 +10,7 @@ use ostd::{Vec, string::ToString, String};
 const KEY_TOTAL_SUPPLY: &'static str = "total_supply";
 const TOTAL_SUPPLY: u64 = 100000000000;
 const KEY_BALANCE : &'static str = "b";
+const KEY_APPROVE : &'static str = "a";
 
 #[ostd::abi_codegen::contract]
 pub trait MyToken {
@@ -17,10 +18,15 @@ pub trait MyToken {
     fn name(&self) -> String;
     fn balance_of(&self, owner: Address) -> U256;
     fn transfer(&mut self, from: Address, to: Address, amount: U256) -> bool;
+    fn approve(&mut self, approves: Address, receiver: Address, amount:U256) -> bool;
+    fn transfer_from(&mut self, receiver:Address,approves:Address, amount:U256) -> bool;
+    fn allowance(&mut self, approves:Address, receiver:Address) -> U256;
     fn total_supply(&self) -> U256;
 
     #[event]
     fn Transfer(&self, from: Address, to: Address, amount: U256) {}
+    #[event]
+    fn Approve(&self, approves:Address, receiver:Address, amount:U256) {}
 }
 
 pub(crate) struct MyTokenInstance;
@@ -60,7 +66,43 @@ impl MyToken for MyTokenInstance {
             true
         }
     }
-
+    fn approve(&mut self, approves: Address, receiver: Address, amount:U256) -> bool {
+        if runtime::check_witness(&approves) == false {
+            return false;
+        }
+        let apprbal = self.balance_of(approves.clone());
+        if apprbal < amount {
+            return false;
+        } else {
+            database::put(&utils::gen_approve_key(&approves, &receiver), amount);
+            self.Approve(approves, receiver, amount);
+            true
+        }
+    }
+    fn transfer_from(&mut self, receiver:Address,approves:Address, amount:U256) -> bool {
+        if runtime::check_witness(&receiver) == false {
+            return false;
+        }
+        let mut allow = self.allowance(approves.clone(), receiver.clone());
+        if allow < amount {
+            return false;
+        }
+        let mut approbal = self.balance_of(approves.clone());
+        if approbal < allow {
+            return false;
+        }
+        let mut receivbal = self.balance_of(receiver.clone());
+        receivbal = receivbal + amount;
+        approbal = approbal - amount;
+        allow = allow - amount;
+        database::put(utils::gen_approve_key(&approves, &receiver), allow);
+        database::put(utils::gen_balance_key(&approves), approbal);
+        database::put(utils::gen_balance_key(&receiver), receivbal);
+        true
+    }
+    fn allowance(&mut self, approves:Address, receiver:Address) -> U256 {
+        database::get(&utils::gen_approve_key(&approves, &receiver)).unwrap_or(U256::zero())
+    }
     fn total_supply(&self) -> U256 {
         database::get(KEY_TOTAL_SUPPLY).unwrap()
     }
@@ -78,6 +120,12 @@ mod utils {
         let mut key =  KEY_BALANCE.as_bytes().to_vec();
         key.extend_from_slice(addr.as_ref());
 
+        key
+    }
+    pub fn gen_approve_key(approves:&Address, receiver:&Address) -> Vec<u8> {
+        let mut key:Vec<u8> = KEY_APPROVE.as_bytes().to_vec();
+        key.extend_from_slice(approves.as_ref());
+        key.extend_from_slice(receiver.as_ref());
         key
     }
 }
