7f1c12c5a800c92d6800328b7f8098ba647ab699
lucas7788 <sishsh@163.com>
2019-01-24 10:52:50 +0800

    fix test  append_witness bug

--- examples/token-codegen/src/test.rs
+++ examples/token-codegen/src/test.rs
@@ -40,12 +40,11 @@ fn transfer() {
 fn approve() {
     let owner = Address::zero();
     let alice = Address::repeat_byte(1);
-    setup_runtime(RuntimeBuilder::default().append_witness(&owner).build());
+    setup_runtime(RuntimeBuilder::default().append_witness(&owner).append_witness(&alice).build());
     let mut token = MyTokenInstance;
     assert!(token.initialize(owner.clone()));
     assert!(token.approve(owner.clone(), alice.clone(), U256::from(100)));
     assert_eq!(token.allowance(owner.clone(), alice.clone()), U256::from(100));
-    setup_runtime(RuntimeBuilder::default().append_witness(&owner).append_witness(&alice).build());
     assert!(token.transfer_from(alice.clone(), owner.clone(), U256::from(100)));
     assert_eq!(token.allowance(owner.clone(), alice.clone()), U256::from(0));
 }
