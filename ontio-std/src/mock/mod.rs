c073f46e961fc6e5caf9c44993742826d59e40af
laizy <laizy@users.noreply.github.com>
2019-01-28 19:31:27 +0800

    adjust misleading mock api (#17)

--- ontio-std/src/mock/mod.rs
+++ ontio-std/src/mock/mod.rs
@@ -1,12 +1,12 @@
 mod runtime;
 
-pub use self::runtime::setup_runtime;
+use self::runtime::setup_runtime;
 pub use self::runtime::Runtime;
-
-use self::runtime::RuntimeImpl;
+use self::runtime::RuntimeInner;
 use crate::types::Address;
-use std::collections::HashMap;
 use std::cell::RefCell;
+use std::rc::Rc;
+use std::iter::Iterator;
 use crate::abi::{Encoder, Sink};
 
 #[cfg(test)]
@@ -17,58 +17,59 @@ mod tests {
     }
 }
 
-#[derive(Default)]
-pub struct RuntimeBuilder {
-    storage: HashMap<Vec<u8>, Vec<u8>>,
-    timestamp: u64,
-    block_height: u64,
-    caller: Address,
-    witness: Vec<Address>,
+pub struct RuntimeHandle {
+   inner: Rc<RefCell<RuntimeInner>>,
 }
 
-impl RuntimeBuilder {
-    pub fn storage_put<K: AsRef<[u8]>, T:Encoder>(mut self, key: K, val: T) ->Self {
+impl RuntimeHandle {
+    pub fn storage_put<K: AsRef<[u8]>, T:Encoder>(&self, key: K, val: T) -> &Self {
         let mut sink = Sink::new(12);
         sink.write(val);
-        self.storage.insert(key.as_ref().to_vec(), sink.into());
+        self.inner.borrow_mut().storage.insert(key.as_ref().to_vec(), sink.into());
         self
     }
 
-    pub fn timestamp(mut self, time: u64) ->Self {
-        self.timestamp = time;
+    pub fn storage_read(&self, key: &[u8]) -> Option<Vec<u8>> {
+        self.inner.borrow().storage.get(key).map(|val| val.to_vec())
+    }
+
+    pub fn storage_delete(&self, key: &[u8]) -> &Self {
+        self.inner.borrow_mut().storage.remove(key);
         self
     }
 
-    pub fn block_height(mut self, height: u64) ->Self {
-        self.block_height = height;
+    pub fn timestamp(&self, time: u64) ->&Self {
+        self.inner.borrow_mut().timestamp = time;
         self
     }
 
-    pub fn address(mut self, addr: &Address) ->Self {
-        self.caller = addr.clone();
+    pub fn block_height(&self, height: u64) ->&Self {
+        self.inner.borrow_mut().block_height = height;
         self
     }
 
-    pub fn caller(mut self, caller: &Address) ->Self {
-        self.caller= caller.clone();
+    pub fn address(&self, addr: &Address) ->&Self {
+        self.inner.borrow_mut().self_addr = addr.clone();
         self
     }
 
-    pub fn append_witness(mut self, addr: &Address) -> Self {
-        self.witness.push(addr.clone());
+    pub fn caller(&self, caller: &Address) ->&Self {
+        self.inner.borrow_mut().caller = caller.clone();
         self
     }
 
-    pub fn build(self) -> Box<Runtime> {
-        let rt = RuntimeImpl {
-            storage: RefCell::new(self.storage),
-            timestamp: self.timestamp,
-            block_height: self.block_height,
-            caller: self.caller,
-            witness: self.witness,
-            notify: RefCell::new(Vec::new()),
-        };
-
-        Box::new(rt)
+    pub fn witness<T : AsRef<Address>, I:IntoIterator<Item = T>>(&self, addr: I) -> &Self {
+        self.inner.borrow_mut().witness = addr.into_iter().map(|a| a.as_ref().clone()).collect();
+        self
     }
+}
+
+pub fn build_runtime() -> RuntimeHandle {
+    let inner = Rc::new(RefCell::new(RuntimeInner::default()));
+
+    let rt = Runtime { inner: inner.clone() };
+    setup_runtime(rt);
+
+    let handle = RuntimeHandle { inner: inner };
+    handle
 }
--- ontio-std/src/mock/runtime.rs
+++ ontio-std/src/mock/runtime.rs
@@ -1,73 +1,66 @@
 use crate::types::Address;
 use std::cell::RefCell;
+use std::rc::Rc;
 use std::collections::HashMap;
 
 /// Mock of contract execution runtime
-pub trait Runtime {
-    fn storage_write(&self, key: &[u8], val: &[u8]);
-    fn storage_read(&self, key: &[u8]) -> Option<Vec<u8>>;
-    fn storage_delete(&self, key: &[u8]) ;
-    fn timestamp(&self) -> u64;
-    fn block_height(&self) -> u64;
-    fn address(&self) -> Address;
-    fn caller(&self) -> Address;
-    fn check_witness(&self, addr: &Address) -> bool;
-    fn notify(&self, msg: &[u8]);
-    //    fn input() -> Vec<u8> ;
-    //    fn ret(data: &[u8]) -> ! ;
+#[derive(Default)]
+pub struct Runtime{
+    pub(crate) inner: Rc<RefCell<RuntimeInner>>,
 }
 
 #[derive(Default)]
-pub(crate) struct RuntimeImpl {
-    pub(crate) storage: RefCell<HashMap<Vec<u8>, Vec<u8>>>,
+pub(crate) struct RuntimeInner{
+    pub(crate) storage: HashMap<Vec<u8>, Vec<u8>>,
     pub(crate) timestamp: u64,
     pub(crate) block_height: u64,
     pub(crate) caller: Address,
+    pub(crate) self_addr:Address,
     pub(crate) witness: Vec<Address>,
-    pub(crate) notify: RefCell<Vec<Vec<u8>>>,
+    pub(crate) notify: Vec<Vec<u8>>,
 }
 
-impl Runtime for RuntimeImpl {
+impl Runtime {
     fn storage_write(&self, key: &[u8], val: &[u8]) {
-        self.storage.borrow_mut().insert(key.into(), val.to_vec());
+        self.inner.borrow_mut().storage.insert(key.into(), val.to_vec());
     }
 
     fn storage_read(&self, key: &[u8]) -> Option<Vec<u8>> {
-        self.storage.borrow().get(key).map(|val| val.to_vec())
+        self.inner.borrow().storage.get(key).map(|val| val.to_vec())
     }
 
     fn storage_delete(&self, key: &[u8]) {
-        self.storage.borrow_mut().remove(key);
+        self.inner.borrow_mut().storage.remove(key);
     }
 
     fn timestamp(&self) -> u64 {
-        self.timestamp
+        self.inner.borrow().timestamp
     }
 
     fn block_height(&self) -> u64 {
-        self.block_height
+        self.inner.borrow().block_height
     }
 
     fn address(&self) -> Address {
-        self.caller.clone()
+        self.inner.borrow().self_addr.clone()
     }
 
     fn caller(&self) -> Address {
-        self.caller.clone()
+        self.inner.borrow().caller.clone()
     }
 
     fn check_witness(&self, addr: &Address) -> bool {
-        self.witness.iter().position(|wit| wit == addr).is_some()
+        self.inner.borrow().witness.iter().position(|wit| wit == addr).is_some()
     }
 
     fn notify(&self, msg: &[u8]) {
-        self.notify.borrow_mut().push(msg.to_vec());
+        self.inner.borrow_mut().notify.push(msg.to_vec());
     }
 }
 
-thread_local!(static RUNTIME: RefCell<Box<dyn Runtime>> = RefCell::new(Box::new(RuntimeImpl::default())));
+thread_local!(static RUNTIME: RefCell<Runtime> = RefCell::new(Runtime::default()));
 
-pub fn setup_runtime(runtime: Box<dyn Runtime>) {
+pub fn setup_runtime(runtime: Runtime) {
     RUNTIME.with(|r| *r.borrow_mut() = runtime);
 }
 
@@ -141,13 +134,5 @@ mod env {
     pub unsafe fn notify(ptr: *const u8, len: u32) {
         let msg = slice::from_raw_parts(ptr, len as usize);
         RUNTIME.with(|r| r.borrow().notify(msg));
-
     }
-
-//        pub fn ret(ptr: *const u8, len: u32) -> !;
-//        pub fn input_length() -> u32;
-//        pub fn get_input(dst: *mut u8);
-//
-
-
 }
