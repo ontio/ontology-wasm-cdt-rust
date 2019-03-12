c19ee4195c0d1264bc125e2192e0e4bb449ee47b
laizy <aochyi@126.com>
2019-03-12 15:21:40 +0800

    fmt

--- examples/apitest/src/lib.rs
+++ examples/apitest/src/lib.rs
@@ -1,14 +1,14 @@
-#![cfg_attr(not(feature="mock"), no_std)]
+#![cfg_attr(not(feature = "mock"), no_std)]
 #![feature(proc_macro_hygiene)]
 
 extern crate ontio_std as ostd;
-use ostd::prelude::*;
 use ostd::abi::Dispatcher;
-use ostd::{runtime, console};
-use ostd::abi::{Sink, Source, Decoder};
-use ostd::types::{to_neo_bytes};
+use ostd::abi::{Decoder, Sink, Source};
 use ostd::contract::ont;
+use ostd::prelude::*;
 use ostd::str;
+use ostd::types::to_neo_bytes;
+use ostd::{console, runtime};
 
 #[ostd::abi_codegen::contract]
 pub trait ApiTest {
@@ -17,21 +17,30 @@ pub trait ApiTest {
     fn self_address(&self) -> Address;
     fn caller_address(&self) -> Address;
     fn entry_address(&self) -> Address;
-    fn contract_debug(&self, content:&str) ->();
+    fn contract_debug(&self, content: &str) -> ();
     //    fn contract_delete(&self) -> ();
     fn check_witness(&self, addr: &Address) -> bool;
     fn get_current_blockhash(&self) -> H256;
     fn get_current_txhash(&self) -> H256;
-    fn call_wasm_name(&self, contract_address:&Address) -> String;
-    fn call_wasm_balance_of(&self, contract_address:&Address, addr:&Address) -> U256;
-    fn call_wasm_transfer(&self, contract_address:&Address, from: &Address, to:&Address, amount:U256) -> bool;
-    fn call_neovm_transfer(&self, contract_address:&Address, from:&Address, to:&Address, amount:U256) -> bool;
-    fn call_ont_transfer(&self, from: &Address, to:&Address, amount:U256) -> bool;
-    fn call_ont_balance_of(&self,address:&Address) -> U256;
-    fn call_ont_approve(&self,from: &Address, to:&Address, amount:U256) -> bool;
-    fn call_ont_allowance(&self, from: &Address, to:&Address) -> U256;
-    fn call_ont_transfer_from(&self,sender: &Address, from: &Address, to:&Address, amount:U256) -> bool;
-    fn contract_migrate(&self, code: Vec<u8>, vm_type: u32, name:&str, version:&str,author: &str, email:&str, desc:&str) -> bool;
+    fn call_wasm_name(&self, contract_address: &Address) -> String;
+    fn call_wasm_balance_of(&self, contract_address: &Address, addr: &Address) -> U256;
+    fn call_wasm_transfer(
+        &self, contract_address: &Address, from: &Address, to: &Address, amount: U256,
+    ) -> bool;
+    fn call_neovm_transfer(
+        &self, contract_address: &Address, from: &Address, to: &Address, amount: U256,
+    ) -> bool;
+    fn call_ont_transfer(&self, from: &Address, to: &Address, amount: U256) -> bool;
+    fn call_ont_balance_of(&self, address: &Address) -> U256;
+    fn call_ont_approve(&self, from: &Address, to: &Address, amount: U256) -> bool;
+    fn call_ont_allowance(&self, from: &Address, to: &Address) -> U256;
+    fn call_ont_transfer_from(
+        &self, sender: &Address, from: &Address, to: &Address, amount: U256,
+    ) -> bool;
+    fn contract_migrate(
+        &self, code: Vec<u8>, vm_type: u32, name: &str, version: &str, author: &str, email: &str,
+        desc: &str,
+    ) -> bool;
 }
 
 pub(crate) struct ApiTestInstance;
@@ -52,12 +61,12 @@ impl ApiTest for ApiTestInstance {
     fn entry_address(&self) -> Address {
         runtime::entry_address()
     }
-    fn contract_debug(&self, content:&str) {
+    fn contract_debug(&self, content: &str) {
         console::debug(content);
     }
     //    fn contract_delete(&self) -> () {
-//        runtime::contract_delete();
-//    }
+    //        runtime::contract_delete();
+    //    }
     fn check_witness(&self, addr: &Address) -> bool {
         let b = runtime::check_witness(addr);
         if b {
@@ -74,7 +83,7 @@ impl ApiTest for ApiTestInstance {
     fn get_current_txhash(&self) -> H256 {
         runtime::current_txhash()
     }
-    fn call_wasm_name(&self, contract_address:&Address) -> String {
+    fn call_wasm_name(&self, contract_address: &Address) -> String {
         let mut sink = Sink::new(16);
         sink.write("name".to_string());
         console::debug(&format!("{:?}", contract_address));
@@ -84,29 +93,33 @@ impl ApiTest for ApiTestInstance {
         let mut source = Source::new(res);
         source.read().unwrap()
     }
-    fn call_wasm_balance_of(&self, contract_address:&Address, addr:&Address) -> U256 {
+    fn call_wasm_balance_of(&self, contract_address: &Address, addr: &Address) -> U256 {
         let mut sink = Sink::new(16);
         sink.write(("balance_of".to_string(), addr));
         let res = runtime::call_contract(contract_address, sink.into().as_slice());
         if res.is_some() {
             let temp = res.unwrap();
-            let mut source =Source::new(temp);
+            let mut source = Source::new(temp);
             U256::decode(&mut source).unwrap()
         } else {
             U256::zero()
         }
     }
-    fn call_wasm_transfer(&self, contract_address:&Address, from: &Address, to:&Address, amount:U256) -> bool {
+    fn call_wasm_transfer(
+        &self, contract_address: &Address, from: &Address, to: &Address, amount: U256,
+    ) -> bool {
         let mut sink = Sink::new(16);
-        sink.write(("transfer".to_string(),from, to, amount));
-        let res = runtime::call_contract(contract_address,sink.into().as_slice());
+        sink.write(("transfer".to_string(), from, to, amount));
+        let res = runtime::call_contract(contract_address, sink.into().as_slice());
         if res.is_some() {
             true
         } else {
             false
         }
     }
-    fn call_neovm_transfer(&self, contract_address:&Address, from:&Address, to:&Address, amount:U256) -> bool {
+    fn call_neovm_transfer(
+        &self, contract_address: &Address, from: &Address, to: &Address, amount: U256,
+    ) -> bool {
         let mut sink = Sink::new(16);
         sink.write(to_neo_bytes(amount));
         sink.write_neovm_address(to);
@@ -116,7 +129,7 @@ impl ApiTest for ApiTestInstance {
         sink.write("transfer".to_string());
         sink.write(103u8);
         sink.write(contract_address);
-        let res = runtime::call_contract(contract_address,sink.into().as_slice());
+        let res = runtime::call_contract(contract_address, sink.into().as_slice());
         if res.is_some() {
             let data = res.unwrap();
             runtime::notify("true".as_bytes());
@@ -127,36 +140,36 @@ impl ApiTest for ApiTestInstance {
             false
         }
     }
-    fn call_ont_transfer(&self,from: &Address, to:&Address, amount:U256) -> bool {
-        let state = ont::State{
-            from:from.clone(),
-            to:to.clone(),
-            amount:amount,
-        };
+    fn call_ont_transfer(&self, from: &Address, to: &Address, amount: U256) -> bool {
+        let state = ont::State { from: from.clone(), to: to.clone(), amount: amount };
         ont::transfer(&[state])
     }
-    fn call_ont_approve(&self,from: &Address, to:&Address, amount:U256) -> bool {
+    fn call_ont_approve(&self, from: &Address, to: &Address, amount: U256) -> bool {
         ont::approve(from, to, amount)
     }
-    fn call_ont_allowance(&self, from: &Address, to:&Address) -> U256 {
+    fn call_ont_allowance(&self, from: &Address, to: &Address) -> U256 {
         ont::allowance(from, to)
     }
-    fn call_ont_balance_of(&self,address:&Address) -> U256 {
+    fn call_ont_balance_of(&self, address: &Address) -> U256 {
         ont::balance_of(address)
     }
-    fn call_ont_transfer_from(&self,sender: &Address, from: &Address, to:&Address, amount:U256) -> bool {
-        ont::transfer_from(sender,from,to,amount)
+    fn call_ont_transfer_from(
+        &self, sender: &Address, from: &Address, to: &Address, amount: U256,
+    ) -> bool {
+        ont::transfer_from(sender, from, to, amount)
     }
-    fn contract_migrate(&self, code: Vec<u8>, vm_type: u32, name:&str, version:&str,author: &str, email:&str, desc:&str) -> bool {
-        runtime::contract_migrate(code.as_slice(), vm_type, name,version,author, email, desc);
+    fn contract_migrate(
+        &self, code: Vec<u8>, vm_type: u32, name: &str, version: &str, author: &str, email: &str,
+        desc: &str,
+    ) -> bool {
+        runtime::contract_migrate(code.as_slice(), vm_type, name, version, author, email, desc);
         true
     }
 }
 
-
 #[no_mangle]
 pub fn invoke() {
-    let mut dispatcher =  ApiTestDispatcher::new(ApiTestInstance);
+    let mut dispatcher = ApiTestDispatcher::new(ApiTestInstance);
     runtime::ret(&dispatcher.dispatch(&runtime::input()));
 }
 
--- examples/apitest/src/test.rs
+++ examples/apitest/src/test.rs
@@ -1,9 +1,9 @@
 extern crate hexutil;
 extern crate ontio_std as ostd;
-use crate::{ApiTest,ApiTestInstance};
+use crate::{ApiTest, ApiTestInstance};
+use ostd::mock::build_runtime;
 use ostd::types::{Address, H256};
 use ostd::vec::Vec;
-use ostd::mock::build_runtime;
 
 const _from: Address = ostd::base58!("AeJGmTDUdSzMdrSHU2pa8rLMo23AAs53LM");
 
@@ -20,24 +20,21 @@ fn call_transfer() {
 
     let self_address = Address::random();
     build_runtime().address(&self_address);
-    assert_eq!(api.self_address(),  self_address);
+    assert_eq!(api.self_address(), self_address);
 
     let caller_address = Address::random();
     build_runtime().caller(&caller_address);
-    assert_eq!(api.caller_address(),  caller_address);
+    assert_eq!(api.caller_address(), caller_address);
 
     let entry_address = Address::random();
     build_runtime().entry_address(&entry_address);
-    assert_eq!(api.entry_address(),  entry_address);
+    assert_eq!(api.entry_address(), entry_address);
 
-    let current_block_hash = H256::new([0u8;32]);
+    let current_block_hash = H256::new([0u8; 32]);
     build_runtime().current_blockhash(&current_block_hash);
-    assert_eq!(api.get_current_blockhash(),  current_block_hash);
+    assert_eq!(api.get_current_blockhash(), current_block_hash);
 
-    let current_tx_hash = H256::new([0u8;32]);
+    let current_tx_hash = H256::new([0u8; 32]);
     build_runtime().current_txhash(&current_tx_hash);
-    assert_eq!(api.get_current_txhash(),  current_tx_hash);
+    assert_eq!(api.get_current_txhash(), current_tx_hash);
 }
-
-
-
