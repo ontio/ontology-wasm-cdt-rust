db69ad7ee1575f7f239e4d23a2f284b63837dce0
laizy <aochyi@126.com>
2019-03-16 12:30:22 +0800

    add zero_copy_source

---
+++ examples/token-zero-copy/src/lib.rs
@@ -0,0 +1,76 @@
+#![no_std]
+extern crate ontio_std as ostd;
+
+use ostd::abi::{Encoder, Sink, Source, ZeroCopySource};
+use ostd::types::{Address, Addr, U256};
+use ostd::{database, runtime};
+use ostd::{string::ToString, String};
+
+const KEY_TOTAL_SUPPLY: &'static str = "total_supply";
+const NAME: &'static str = "wasm_token";
+const SYMBOL: &'static str = "WTK";
+const TOTAL_SUPPLY: u64 = 100000000000;
+
+fn initialize() -> bool {
+    database::put(KEY_TOTAL_SUPPLY, U256::from(TOTAL_SUPPLY));
+    true
+}
+
+fn balance_of(owner: &Addr) -> U256 {
+    database::get(owner).unwrap_or(U256::zero())
+}
+
+fn transfer(from: &Addr, to: &Addr, amount: U256) -> bool {
+    if runtime::check_witness(&from) == false {
+        return false;
+    }
+    let mut frmbal = balance_of(&from);
+    let mut tobal = balance_of(&to);
+    if amount == 0.into() || frmbal < amount {
+        false
+    } else {
+        frmbal = frmbal - amount;
+        tobal = tobal + amount;
+        database::put(&from, frmbal);
+        database::put(&to, tobal);
+        notify(("Transfer".to_string(), from, to, amount));
+        true
+    }
+}
+
+fn total_supply() -> U256 {
+    database::get(KEY_TOTAL_SUPPLY).unwrap()
+}
+
+#[no_mangle]
+pub fn invoke() {
+    let input = runtime::input();
+    let mut source = ZeroCopySource::new(&input);
+    let action = source.next_var_bytes().unwrap();
+    let mut sink = Sink::new(12);
+    match action {
+        b"init" => sink.write(initialize()),
+        b"name" => sink.write(NAME.to_string()),
+        b"symbol" => sink.write(SYMBOL.to_string()),
+        b"totalSupply" => sink.write(total_supply()),
+        b"balanceOf" => {
+            let addr = source.next_addr().unwrap();
+            sink.write(balance_of(&addr));
+        }
+        b"transfer" => {
+            let from = source.next_addr().unwrap();
+            let to = source.next_addr().unwrap();
+            let amount = U256::zero();
+            sink.write(transfer(from, to, amount));
+        }
+        _ => panic!("unsupported action!"),
+    }
+
+    runtime::ret(&sink.into())
+}
+
+fn notify<T: Encoder>(msg: T) {
+    let mut sink = Sink::new(16);
+    sink.write(msg);
+    runtime::notify(&sink.into());
+}
---
+++ ontio-std/src/abi/zero_copy_source.rs
@@ -0,0 +1,114 @@
+use super::Decoder;
+use super::Error;
+use crate::Vec;
+use byteorder::{ByteOrder, LittleEndian};
+
+use crate::types::Addr;
+
+fn varuint_encode_size(val: u64) -> usize {
+    if val < 0xfd {
+        1
+    } else if val <= 0xffff {
+        3
+    } else if val <= 0xFFFFFFFF {
+        5
+    } else {
+        9
+    }
+}
+
+pub struct ZeroCopySource<'a> {
+    buf: &'a [u8],
+    pos: usize,
+}
+
+impl<'a> ZeroCopySource<'a> {
+    pub fn new(data: &'a [u8]) -> Self {
+        Self { buf: data, pos: 0 }
+    }
+
+    pub(crate) fn next_bytes(&mut self, len: usize) -> Result<&'a [u8], Error> {
+        if self.buf.len() - self.pos < len {
+            Err(Error::UnexpectedEOF)
+        } else {
+            let bytes = &self.buf[self.pos..self.pos + len];
+            self.pos += len;
+            Ok(bytes)
+        }
+    }
+
+    pub fn next_var_bytes(&mut self)->  Result<&'a [u8], Error> {
+        let n = self.read_varuint()?;
+        self.next_bytes(n as usize)
+    }
+
+    pub fn next_addr(&mut self) -> Result<&'a Addr, Error> {
+        let buf = self.next_bytes(20)?;
+        Ok(Addr::from_u8_slice(buf))
+    }
+
+    pub(crate) fn read_into(&mut self, buf: &mut [u8]) -> Result<(), Error> {
+        let bytes = self.next_bytes(buf.len())?;
+        buf.copy_from_slice(bytes);
+        Ok(())
+    }
+
+    pub(crate) fn read_byte(&mut self) -> Result<u8, Error> {
+        if self.pos >= self.buf.len() {
+            Err(Error::UnexpectedEOF)
+        } else {
+            let b = self.buf[self.pos];
+            self.pos += 1;
+            Ok(b)
+        }
+    }
+
+    pub(crate) fn read_bool(&mut self) -> Result<bool, Error> {
+        match self.read_byte()? {
+            0 => Ok(false),
+            1 => Ok(true),
+            _ => Err(Error::IrregularData),
+        }
+    }
+
+    #[allow(unused)]
+    pub(crate) fn skip(&mut self, n: usize) -> Result<(), Error> {
+        if self.buf.len() - self.pos < n {
+            Err(Error::UnexpectedEOF)
+        } else {
+            self.pos += n;
+            Ok(())
+        }
+    }
+
+    #[allow(unused)]
+    pub(crate) fn backup(&mut self, n: usize) {
+        assert!(self.pos >= n);
+        self.pos -= n;
+    }
+
+    pub(crate) fn read_u16(&mut self) -> Result<u16, Error> {
+        Ok(LittleEndian::read_u16(self.next_bytes(2)?))
+    }
+
+    pub(crate) fn read_u32(&mut self) -> Result<u32, Error> {
+        Ok(LittleEndian::read_u32(self.next_bytes(4)?))
+    }
+
+    pub(crate) fn read_u64(&mut self) -> Result<u64, Error> {
+        Ok(LittleEndian::read_u64(self.next_bytes(8)?))
+    }
+
+    pub(crate) fn read_varuint(&mut self) -> Result<u64, Error> {
+        match self.read_byte()? {
+            0xFD => self.read_u16().map(|v| (3, v as u64)),
+            0xFE => self.read_u32().map(|v| (5, v as u64)),
+            0xFF => self.read_u64().map(|v| (9, v)),
+            val => Ok((1, val as u64)),
+        }
+        .and_then(|(len, val)| match len == varuint_encode_size(val) {
+            true => Ok(val),
+            false => Err(Error::IrregularData),
+        })
+    }
+}
