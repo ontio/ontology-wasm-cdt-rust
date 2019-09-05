f7626330ed0c6d00e3afe25f27ff24f0d083fa12
laizy <aochyi@126.com>
2019-09-05 19:35:18 +0800

    rename debug to ontio_debug

--- ontio-std/src/console.rs
+++ ontio-std/src/console.rs
@@ -1,11 +1,11 @@
 mod env {
     extern "C" {
-        pub fn debug(data: *const u8, len: u32);
+        pub fn ontio_debug(data: *const u8, len: u32);
     }
 }
 
 pub fn debug(msg: &str) {
     unsafe {
-        env::debug(msg.as_ptr(), msg.len() as u32);
+        env::ontio_debug(msg.as_ptr(), msg.len() as u32);
     }
 }
