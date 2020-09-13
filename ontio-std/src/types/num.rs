298334cf350fa13448bbbe2833b78fff8466e66b
laizy <aochyi@126.com>
2020-09-13 15:44:33 +0800

    format

--- ontio-std/src/types/num.rs
+++ ontio-std/src/types/num.rs
@@ -60,21 +60,20 @@ impl Sum for U128 {
     }
 }
 
-impl Sum<&U128> for U128 {
-    fn sum<I: Iterator<Item = &Self>>(iter: I) -> Self {
+impl<'a> Sum<&'a U128> for U128 {
+    fn sum<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
         iter.fold(U128::new(0), Add::add)
     }
 }
 
-impl Add<&U128> for U128 {
+impl<'a> Add<&'a U128> for U128 {
     type Output = U128;
 
     #[track_caller]
-    fn add(self, rhs: &U128) -> Self::Output {
+    fn add(self, rhs: &'a U128) -> Self::Output {
         U128(self.0.checked_add(rhs.0).unwrap())
     }
 }
-
 
 impl Add<U128> for U128 {
     type Output = U128;
