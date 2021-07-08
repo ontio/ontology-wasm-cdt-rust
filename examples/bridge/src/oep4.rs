ace97b817f50fa7d7b9cac13a661b3fc156a0f41
lucas <sishsh@163.com>
2021-07-08 11:09:02 +0800

    add bridge contract example (#113)
    * update
    
    * update name
    
    * update
    
    * update
    
    * fix
    
    * fmt code
    
    * update event
    
    * add amount decimals transfer
    
    * fix bug
    
    * remove decimals check
    
    * fix
    
    * fix decimal transfer bug
    
    * update
    
    Co-authored-by: laizy <aochyi@126.com>

--- examples/bridge/src/oep4.rs
+++ examples/bridge/src/oep4.rs
@@ -22,21 +22,21 @@ pub fn balance_of_neovm(contract: &Address, account: &Address) -> U128 {
     call_neovm_num(contract, builder.bytes().as_slice())
 }
 
-pub fn transfer_neovm(contract: &Address, from: &Address, to: &Address, amount: U128) -> bool {
+pub fn transfer_neovm(contract: &Address, from: &Address, to: &Address, amount: U128) {
     if contract == &ONT_CONTRACT_ADDRESS {
-        return ont::transfer(from, to, amount);
+        assert!(ont::transfer(from, to, amount), "ont transfer failed");
+    } else if contract == &ONG_CONTRACT_ADDRESS {
+        assert!(ong::transfer(from, to, amount), "ong transfer failed");
+    } else {
+        let mut builder = VmValueBuilder::new();
+        builder.string("transfer");
+        let mut nested = builder.list();
+        nested.address(from);
+        nested.address(to);
+        nested.number(amount);
+        nested.finish();
+        assert!(call_neovm_bool(contract, builder.bytes().as_slice()), "oep4 transfer failed");
     }
-    if contract == &ONG_CONTRACT_ADDRESS {
-        return ong::transfer(from, to, amount);
-    }
-    let mut builder = VmValueBuilder::new();
-    builder.string("transfer");
-    let mut nested = builder.list();
-    nested.address(from);
-    nested.address(to);
-    nested.number(amount);
-    nested.finish();
-    call_neovm_bool(contract, builder.bytes().as_slice())
 }
 
 #[track_caller]
