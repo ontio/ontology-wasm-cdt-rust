1c63297385b5f6b95977658e7183f3e558dfa14d
lucas7788 <sishsh@163.com>
2021-10-26 17:01:24 +0800

    add ont ong decimals

--- examples/call_native/src/lib.rs
+++ examples/call_native/src/lib.rs
@@ -1,9 +1,12 @@
 #![no_std]
 #![feature(proc_macro_hygiene)]
 extern crate ontio_std as ostd;
+
 use ostd::abi::{Sink, Source};
-use ostd::contract::ontid;
+use ostd::contract::ong_v2;
+use ostd::contract::ont_v2;
 use ostd::runtime;
+use ostd::runtime::panic;
 
 #[no_mangle]
 pub fn invoke() {
@@ -12,11 +15,48 @@ pub fn invoke() {
     let action: &[u8] = source.read().unwrap();
     let mut sink = Sink::new(12);
     match action {
-        b"verifySignature" => {
-            let (ont_id, index) = source.read().unwrap();
-            sink.write(ontid::verify_signature(ont_id, index));
+        b"ontTransferV2" => {
+            let (from, to, amount) = source.read().unwrap();
+            sink.write(ont_v2::transfer(from, to, amount));
         }
-        _ => panic!("unsupported action!"),
+        b"ontBalanceOfV2" => {
+            let from = source.read().unwrap();
+            sink.write(ont_v2::balance_of(from));
+        }
+        b"ontApproveV2" => {
+            let (from, to, amount) = source.read().unwrap();
+            sink.write(ont_v2::approve(from, to, amount));
+        }
+        b"ontAllowanceV2" => {
+            let (from, to) = source.read().unwrap();
+            sink.write(ont_v2::allowance(from, to));
+        }
+        b"ontTransferFromV2" => {
+            let (spender, from, to, amount) = source.read().unwrap();
+            sink.write(ont_v2::transfer_from(spender, from, to, amount));
+        }
+
+        b"ongTransferV2" => {
+            let (from, to, amount) = source.read().unwrap();
+            sink.write(ong_v2::transfer(from, to, amount));
+        }
+        b"ongBalanceOfV2" => {
+            let from = source.read().unwrap();
+            sink.write(ong_v2::balance_of(from));
+        }
+        b"ongApproveV2" => {
+            let (from, to, amount) = source.read().unwrap();
+            sink.write(ong_v2::approve(from, to, amount));
+        }
+        b"ongAllowanceV2" => {
+            let (from, to) = source.read().unwrap();
+            sink.write(ong_v2::allowance(from, to));
+        }
+        b"ongTransferFromV2" => {
+            let (spender, from, to, amount) = source.read().unwrap();
+            sink.write(ong_v2::transfer_from(spender, from, to, amount));
+        }
+        _ => panic("unsupported action!"),
     }
 
     runtime::ret(sink.bytes())
--- ontio-std/src/contract.rs
+++ ontio-std/src/contract.rs
@@ -314,6 +314,115 @@ pub mod ont {
     }
 }
 
+///This module provides the operation API related to ont assets, such as balanceof, transfer, etc.
+pub mod ont_v2 {
+    use crate::macros::base58;
+    use crate::prelude::*;
+
+    const ONT_CONTRACT_ADDRESS: Address = base58!("AFmseVrdL9f9oyCzZefL9tG6UbvhUMqNMV");
+
+    ///Transfer method of ont assets, Transfer ont assets from the from address to the to address
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::ont;
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    ///   let input= input();
+    ///   let mut source = Source::new(&input);
+    ///   let (from, to, amount) = source.read().unwrap();
+    ///   ont::transfer(from,to, amount);
+    /// ```
+    pub fn transfer(from: &Address, to: &Address, val: U128) -> bool {
+        let state = [TransferParam { from: *from, to: *to, amount: val }];
+        super::util::transfer_inner_v2(&ONT_CONTRACT_ADDRESS, state.as_ref())
+    }
+
+    ///transfer_multi method of ont assets,Multiple transfers in one transaction
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::{ont,TransferParam};
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    /// # use ontio_std::types::{Address, U128};
+    ///     let input = input();
+    ///     let mut source = Source::new(&input);
+    ///     let trs: Vec<(Address,Address,U128)> = source.read().unwrap();
+    ///     let mut ts = Vec::<TransferParam>::new();
+    ///     for tr in trs.iter() {
+    ///         let trans = TransferParam{
+    ///            from:tr.0,
+    ///            to:tr.1,
+    ///            amount:tr.2,
+    ///         };
+    ///         ts.push(trans)
+    ///     }
+    ///     ont::transfer_multi(ts.as_slice());
+    /// ```
+    pub fn transfer_multi(transfer: &[TransferParam]) -> bool {
+        super::util::transfer_inner_v2(&ONT_CONTRACT_ADDRESS, transfer)
+    }
+
+    ///from-address can allow to-address to transfer a certain amount of assets from  from-address.
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::{ont,TransferParam};
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    ///   let input = input();
+    ///   let mut source = Source::new(&input);
+    ///   let (from,to,amount) = source.read().unwrap();
+    ///   ont::approve(from, to, amount);
+    /// ```
+    pub fn approve(from: &Address, to: &Address, amount: U128) -> bool {
+        super::util::approve_inner_v2(&ONT_CONTRACT_ADDRESS, from, to, amount)
+    }
+
+    ///Query the balance of ont assets
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::{ont,TransferParam};
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    ///     let input = input();
+    ///     let mut source = Source::new(&input);
+    ///     let addr = source.read().unwrap();
+    ///     ont::balance_of(addr);
+    /// ```
+    pub fn balance_of(address: &Address) -> U128 {
+        super::util::balance_of_inner_v2(&ONT_CONTRACT_ADDRESS, address)
+    }
+
+    ///This method is used in conjunction with the approve method to query the number of approve
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::{ont,TransferParam};
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    ///   let input= input();
+    ///   let mut source = Source::new(&input);
+    ///   let (from, to) = source.read().unwrap();
+    ///   ont::allowance(from,to);
+    /// ```
+    pub fn allowance(from: &Address, to: &Address) -> U128 {
+        super::util::allowance_inner_v2(&ONT_CONTRACT_ADDRESS, from, to)
+    }
+
+    ///Spender transfers a certain amount of ont from from-address to to-address
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::{ont,TransferParam};
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    ///   let input= input();
+    ///   let mut source = Source::new(&input);
+    ///   let (spender, from, to, amount) = source.read().unwrap();
+    ///   ont::transfer_from(spender, from, to, amount);
+    /// ```
+    pub fn transfer_from(sender: &Address, from: &Address, to: &Address, amount: U128) -> bool {
+        super::util::transfer_from_inner_v2(&ONT_CONTRACT_ADDRESS, sender, from, to, amount)
+    }
+}
+
 ///This module provides the operation API related to ong assets, such as balanceof, transfer, etc.
 pub mod ong {
     use crate::prelude::*;
@@ -424,6 +533,116 @@ pub mod ong {
     }
 }
 
+///This module provides the operation API related to ong assets, such as balanceof, transfer, etc.
+pub mod ong_v2 {
+    use crate::prelude::*;
+
+    use crate::macros::base58;
+    use crate::types::{Address, U128};
+
+    const ONG_CONTRACT_ADDRESS: Address = base58!("AFmseVrdL9f9oyCzZefL9tG6UbvhfRZMHJ");
+
+    ///Transfer method of ong assets, Transfer ont assets from the from address to the to address
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::ong;
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    ///   let input = input();
+    ///   let mut source = Source::new(&input);
+    ///   let (from, to, amount) = source.read().unwrap();
+    ///   ong::transfer(from,to, amount);
+    /// ```
+    pub fn transfer(from: &Address, to: &Address, val: U128) -> bool {
+        let state = [TransferParam { from: *from, to: *to, amount: val }];
+        super::util::transfer_inner_v2(&ONG_CONTRACT_ADDRESS, state.as_ref())
+    }
+
+    ///transfer_multi method of ong assets,Multiple transfers in one transaction
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::{ong,TransferParam};
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    /// # use ontio_std::types::{Address,U128};
+    ///     let input = input();
+    ///     let mut source = Source::new(&input);
+    ///     let trs: Vec<(Address,Address,U128)> = source.read().unwrap();
+    ///     let mut transfers = Vec::<TransferParam>::new();
+    ///     for tr in trs.iter() {
+    ///         transfers.push(TransferParam{
+    ///             from:tr.0,
+    ///             to:tr.1,
+    ///             amount:tr.2,
+    ///         })
+    ///     }
+    ///     ong::transfer_multi(transfers.as_slice());
+    /// ```
+    pub fn transfer_multi(transfer: &[super::TransferParam]) -> bool {
+        super::util::transfer_inner_v2(&ONG_CONTRACT_ADDRESS, transfer)
+    }
+
+    ///Query the balance of ong assets
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::{ong,TransferParam};
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    ///     let input = input();
+    ///     let mut source = Source::new(&input);
+    ///     let addr = source.read().unwrap();
+    ///     ong::balance_of(addr);
+    /// ```
+    pub fn balance_of(address: &Address) -> U128 {
+        super::util::balance_of_inner_v2(&ONG_CONTRACT_ADDRESS, address)
+    }
+
+    ///from-address can allow to-address to transfer a certain amount of assets from  from-address.
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::{ong,TransferParam};
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    ///     let input = input();
+    ///     let mut source = Source::new(&input);
+    ///     let (from,to,amount) = source.read().unwrap();
+    ///     ong::approve(from, to, amount);
+    /// ```
+    pub fn approve(from: &Address, to: &Address, amount: U128) -> bool {
+        super::util::approve_inner_v2(&ONG_CONTRACT_ADDRESS, from, to, amount)
+    }
+
+    ///This method is used in conjunction with the approve method to query the number of approve
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::{ong};
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    ///     let input = input();
+    ///     let mut source = Source::new(&input);
+    ///     let (from, to) = source.read().unwrap();
+    ///     ong::allowance(from,to);
+    /// ```
+    pub fn allowance(from: &Address, to: &Address) -> U128 {
+        super::util::allowance_inner_v2(&ONG_CONTRACT_ADDRESS, from, to)
+    }
+
+    ///Spender transfers a certain amount of ong from from-address to to-address
+    /// # Example
+    /// ```no_run
+    /// # use ontio_std::contract::{ong,TransferParam};
+    /// # use ontio_std::abi::{Sink, Source};
+    /// # use ontio_std::runtime::input;
+    ///     let input = input();
+    ///     let mut source = Source::new(&input);
+    ///     let (spender, from, to, amount) = source.read().unwrap();
+    ///     ong::transfer_from(spender, from, to, amount);
+    /// ```
+    pub fn transfer_from(sender: &Address, from: &Address, to: &Address, amount: U128) -> bool {
+        super::util::transfer_from_inner_v2(&ONG_CONTRACT_ADDRESS, sender, from, to, amount)
+    }
+}
+
 pub(crate) mod util {
     use super::super::abi::Sink;
     use super::super::runtime;
@@ -507,6 +726,90 @@ pub(crate) mod util {
         let mut sink_param = Sink::new(64);
         sink_param.write(VERSION);
         sink_param.write("balanceOf");
+        sink_param.write(sink.bytes());
+        let output = runtime::call_contract(contract_address, sink_param.bytes());
+        if !output.is_empty() {
+            return u128_from_neo_bytes(&output);
+        }
+        U128::new(0)
+    }
+
+    pub(crate) fn transfer_inner_v2(
+        contract_address: &Address, transfer: &[super::TransferParam],
+    ) -> bool {
+        let mut sink = Sink::new(64);
+        sink.write_native_varuint(transfer.len() as u64);
+
+        for state in transfer.iter() {
+            sink.write_native_address(&state.from);
+            sink.write_native_address(&state.to);
+            sink.write(u128_to_neo_bytes(state.amount));
+        }
+        let mut sink_param = Sink::new(128);
+        sink_param.write(VERSION);
+        sink_param.write("transferV2");
+        sink_param.write(sink.bytes());
+        let output = runtime::call_contract(contract_address, sink_param.bytes());
+
+        !output.is_empty() && output[0] == 1u8
+    }
+
+    pub(crate) fn approve_inner_v2(
+        contract_address: &Address, from: &Address, to: &Address, amount: U128,
+    ) -> bool {
+        let mut sink = Sink::new(64);
+        sink.write_native_address(from);
+        sink.write_native_address(to);
+        sink.write(u128_to_neo_bytes(amount));
+        let mut sink_param = Sink::new(128);
+        sink_param.write(VERSION);
+        sink_param.write("approveV2");
+        sink_param.write(sink.bytes());
+        let output = runtime::call_contract(contract_address, sink_param.bytes());
+
+        !output.is_empty() && output[0] == 1u8
+    }
+
+    pub(crate) fn transfer_from_inner_v2(
+        contract_address: &Address, sender: &Address, from: &Address, to: &Address, amount: U128,
+    ) -> bool {
+        let mut sink = Sink::new(64);
+        sink.write_native_address(sender);
+        sink.write_native_address(from);
+        sink.write_native_address(to);
+        sink.write(u128_to_neo_bytes(amount));
+        let mut sink_param = Sink::new(128);
+        sink_param.write(VERSION);
+        sink_param.write("transferFromV2");
+        sink_param.write(sink.bytes());
+        let output = runtime::call_contract(contract_address, sink_param.bytes());
+
+        !output.is_empty() && output[0] == 1u8
+    }
+
+    pub(crate) fn allowance_inner_v2(
+        contract_address: &Address, from: &Address, to: &Address,
+    ) -> U128 {
+        let mut sink = Sink::new(64);
+        sink.write_native_address(from);
+        sink.write_native_address(to);
+        let mut sink_param = Sink::new(64);
+        sink_param.write(VERSION);
+        sink_param.write("allowanceV2");
+        sink_param.write(sink.bytes());
+        let output = runtime::call_contract(contract_address, sink_param.bytes());
+        if !output.is_empty() {
+            return u128_from_neo_bytes(&output);
+        }
+        U128::new(0)
+    }
+
+    pub(crate) fn balance_of_inner_v2(contract_address: &Address, address: &Address) -> U128 {
+        let mut sink = Sink::new(64);
+        sink.write_native_address(address);
+        let mut sink_param = Sink::new(64);
+        sink_param.write(VERSION);
+        sink_param.write("balanceOfV2");
         sink_param.write(sink.bytes());
         let output = runtime::call_contract(contract_address, sink_param.bytes());
         if !output.is_empty() {
